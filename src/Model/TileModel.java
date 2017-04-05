f23d55ceb51cdc92ea3f4c875e9937c10681c287
Kurtis J. Taylor <kjtaylor1995@gmail.com>
2017-04-04 23:58:53 -0400

    not much

--- src/Model/TileModel.java
+++ src/Model/TileModel.java
@@ -6,71 +6,72 @@ import javax.swing.*;
 
 public class TileModel extends JButton {
 
-    private final int d = 60; // dimension of tile
-    private final Color cellColor = Color.WHITE; // default color of each cell
-    private String data = ""; 
+	private final int d = 60; // dimension of tile
+	private final Color cellColor = Color.WHITE; // default color of each cell
+	private String data = "";
 
-    /**
-     * Constructor for a Tile object.
-     *
-     * @param fill
-     */
-    public TileModel(boolean fill) {
-        setContentAreaFilled(true);
-        setBorderPainted(true);
-        setOpaque(false);
-        setFont(new Font("Arial", Font.BOLD, 24));
-        setBackground(this.cellColor);
-        setForeground(getRandomColor());
+	/**
+	 * Constructor for a Tile object.
+	 *
+	 * @param fill
+	 */
+	public TileModel(boolean fill) {
+		setContentAreaFilled(true);
+		setBorderPainted(true);
+		setOpaque(false);
+		setFont(new Font("Arial", Font.BOLD, 24));
+		setBackground(this.cellColor);
+		setForeground(getRandomColor());
 
-        if (fill) {
-            Random rand = new Random();
-            int n = rand.nextInt(10); // generate random number in range [1,9]
-            data = "" + n;
-        }
-        
-        setData(data);
-    }
-    
-    public boolean isEmpty() {
-    	return data.equals("");
-    }
-    
-    
-    // set number from a string
-    public void setData(String s){
-        setText("" + s);
-        data="" + s;
-    }
-    
-    // set data from a integer
-    public void setData(int i){
-        setText("" + i);
-        data="" + i;
-    }
-    public void newRandomValue() {
-        Random rand = new Random();
-        int n = rand.nextInt(10); // generate random number in range [1,9]
-        this.setData(n);
-    }
-    // get data and return as string
-    public String getData(){
-    	return data;
-    }
-    
-    private Color getRandomColor() {
-    	Random rand = new Random();
-    	int r = rand.nextInt(255);
-    	int g = rand.nextInt(255);
-    	int b = rand.nextInt(255);
-    	
-    	Color c = new Color(r, g, b);
-    	return c.darker();
-    }
+		if (fill) {
+			Random rand = new Random();
+			int n = rand.nextInt(10); // generate random number in range [1,9]
+			data = "" + n;
+		}
 
-    @Override
-    public Dimension getPreferredSize() {
-        return new Dimension(d, d);
-    }
+		setData(data);
+	}
+
+	public boolean isEmpty() {
+		return data.equals("");
+	}
+
+	// set number from a string
+	public void setData(String s) {
+		data = s;
+		setText(data);
+	}
+
+	// set data from a integer
+	public void setData(int i) {
+		data = "" + i;
+		setText(data);
+	}
+
+	public void newRandomValue() {
+		Random rand = new Random();
+		int n = rand.nextInt(10); // generate random number in range [1,9]
+		this.setData(n);
+	}
+
+	// get data and return as string
+	public String getData() {
+		return data;
+	}
+
+	private Color getRandomColor() {
+		Random rand = new Random();
+		int r = rand.nextInt(255);
+		int g = rand.nextInt(255);
+		int b = rand.nextInt(255);
+
+		Color c = new Color(r, g, b);
+		return c.darker();
+	}
+
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(d, d);
+	}
 
 }
